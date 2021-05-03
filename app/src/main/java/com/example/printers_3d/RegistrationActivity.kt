b66140fe51afc1e5dd65240d318fd6f3dbfa8c43
trebuh001 <hkrupa96@gmail.com>
2021-05-03 16:30:55 +0200

    Repair functionality: "check of login is already busy"

--- app/src/main/java/com/example/printers_3d/RegistrationActivity.kt
+++ app/src/main/java/com/example/printers_3d/RegistrationActivity.kt
@@ -1,5 +1,6 @@
 package com.example.printers_3d
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
@@ -9,25 +10,65 @@ import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
+import com.firebase.ui.auth.ui.email.RegisterEmailFragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_login.Btn_show_pass
 import kotlinx.android.synthetic.main.activity_registration.*
+import kotlinx.android.synthetic.main.activity_user_settings.*
 
 
 class RegistrationActivity : AppCompatActivity() {
     val TAG="Registration TAG"
+
     private val mAuth: FirebaseAuth= FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
     }
 
+
+    fun CheckNameBusy()
+    {
+        val name:String=Et_name_registration.text?.trim().toString()
+        Progress_bar_registration.visibility= View.VISIBLE
+        val database = Firebase.database.reference
+        var a=0
+        val ordersRef = database.child("Users").orderByValue()
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (ds in dataSnapshot.children) {
+
+                    if(ds.child("name").getValue(String::class.java).toString().equals(name))
+                    {
+                        Progress_bar_registration.visibility= View.GONE
+                        Et_name_registration.setError(getString(R.string.toast_nickname_busy))
+                        Et_name_registration.requestFocus()
+                        return
+                    }
+
+                }
+                RegisterUser()
+
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, databaseError.getMessage()) //Don't ignore errors!
+            }
+        }
+        ordersRef.addListenerForSingleValueEvent(valueEventListener)
+    }
+
     fun RegistrationButton(view: View)
     {
+        CheckNameBusy()
+    }
+
+    fun RegisterUser()
+    {
+
         val name:String=Et_name_registration.text?.trim().toString()
         val email:String=Et_email_registration.text?.trim().toString()
         val password:String=Et_password_registration.text?.trim().toString()
@@ -36,7 +77,7 @@ class RegistrationActivity : AppCompatActivity() {
         val description:String=""
         if(check_fields)
         {
-           return
+            return
         }
 
         if(password.equals(password_repeat))
@@ -44,42 +85,42 @@ class RegistrationActivity : AppCompatActivity() {
             mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                       Progress_bar_registration.visibility= View.VISIBLE
+                        Progress_bar_registration.visibility= View.VISIBLE
                         // Sign in success, update UI with the signed-in user's information
                         Log.d(TAG, "createUserWithEmail:success")
 
-                FirebaseDatabase.getInstance().getReference("Users")
+                        FirebaseDatabase.getInstance().getReference("Users")
                             .child(FirebaseAuth.getInstance().uid.toString()).
-                    setValue(User(name, email,description)).addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful)
-                        {
-                            Toast.makeText(this,getString(R.string.registration_sucessful),Toast.LENGTH_LONG).show();
-                            Et_name_registration.text.clear()
-                            Et_email_registration.text.clear()
-                            Et_password_registration.text.clear()
-                            Et_password_registration_repeat.text.clear()
-                            if(mAuth.currentUser!=null)
-                            {
-                                val intent=Intent(this,MainActivity::class.java)
-                                startActivity(intent)
-                                this.finish()
+                            setValue(User(name, email,description)).addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful)
+                                {
+                                    Toast.makeText(this,getString(R.string.registration_sucessful),Toast.LENGTH_LONG).show();
+                                    Et_name_registration.text.clear()
+                                    Et_email_registration.text.clear()
+                                    Et_password_registration.text.clear()
+                                    Et_password_registration_repeat.text.clear()
+                                    if(mAuth.currentUser!=null)
+                                    {
+                                        val intent=Intent(this,MainActivity::class.java)
+                                        startActivity(intent)
+                                        this.finish()
+                                    }
+                                    Progress_bar_registration.visibility= View.GONE
+                                } else {
+                                    Toast.makeText(this,getString(R.string.registration_error),Toast.LENGTH_LONG).show();
+                                    Progress_bar_registration.visibility= View.GONE
+                                }
+
+
                             }
-                            Progress_bar_registration.visibility= View.GONE
-                        } else {
-                            Toast.makeText(this,getString(R.string.registration_error),Toast.LENGTH_LONG).show();
-                            Progress_bar_registration.visibility= View.GONE
-                        }
-
-
-                    }
-                       // val user = auth.currentUser
+                        // val user = auth.currentUser
 
                     } else {
                         Progress_bar_registration.visibility= View.GONE
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
                         Toast.makeText(this, getString(R.string.hint_register_error), Toast.LENGTH_SHORT).show()
-                     //   updateUI(null)
+                        //   updateUI(null)
                     }
 
                     // ...
@@ -92,21 +133,23 @@ class RegistrationActivity : AppCompatActivity() {
             Et_password_registration_repeat.setError(getString(R.string.passwords_not_same))
             return
         }
-
     }
     fun ShowPassword(view: View)
     {
         if(view.getId()==R.id.Btn_show_pass) {
             if(Et_password_registration.getTransformationMethod().equals(
-                    PasswordTransformationMethod.getInstance())){
+                    PasswordTransformationMethod.getInstance()
+                )){
                 Btn_show_pass.setImageResource(R.drawable.eye_not_open)
                 //Show Password
-                Et_password_registration.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                Et_password_registration
+                    .setTransformationMethod(HideReturnsTransformationMethod.getInstance());
             }
             else{
                 Btn_show_pass.setImageResource(R.drawable.eye_open)
                 //Hide Password
-                Et_password_registration.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                Et_password_registration
+                    .setTransformationMethod(PasswordTransformationMethod.getInstance());
             }
         }
         if(view.getId()==R.id.Btn_show_pass_repeat) {
@@ -122,14 +165,16 @@ class RegistrationActivity : AppCompatActivity() {
             else{
                 Btn_show_pass_repeat.setImageResource(R.drawable.eye_open)
                 //Hide Password
-                Et_password_registration_repeat.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                Et_password_registration_repeat
+                    .setTransformationMethod(PasswordTransformationMethod.getInstance());
             }
         }
     }
 
     fun CheckFields(name: String, email: String, password: String, password_repeat: String):Boolean
+
     {
-        var i:Int=0;
+        var i=0;
         if(name.trim().isEmpty())
         {
             Et_name_registration.setError(getString(R.string.field_cant_be_empty))
@@ -141,8 +186,6 @@ class RegistrationActivity : AppCompatActivity() {
             Et_email_registration.setError(getString(R.string.field_cant_be_empty))
             Et_email_registration.requestFocus()
             i++
-
-
         }
         if(password.trim().isEmpty())
         {
@@ -150,6 +193,7 @@ class RegistrationActivity : AppCompatActivity() {
             Et_password_registration.requestFocus()
             i++
         }
+
         if(password_repeat.trim().isEmpty())
         {
             Et_password_registration_repeat.setError(getString(R.string.field_cant_be_empty))
@@ -162,7 +206,6 @@ class RegistrationActivity : AppCompatActivity() {
             Et_name_registration.requestFocus()
             i++
         }
-
         if(password.trim().length<6)
         {
             Et_password_registration.setError(getString(R.string.password_shorter_than_6_char))
@@ -182,6 +225,5 @@ class RegistrationActivity : AppCompatActivity() {
             i++
         }
         return i>0
-
     }
 }
